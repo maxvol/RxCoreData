bf6f5f1224602923afcf229b0cd7e3b410a91b77
Maxim Volgin <maxvol@users.noreply.github.com>
2020-11-14 05:48:25 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RxCoreData",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), // .tvOS(.v9), .watchOS(.v3)
+        .macOS(.v10_10), .iOS(.v10), // .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
         .library(
